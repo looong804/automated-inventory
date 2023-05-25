19796a8cb4737996048a2e46eb59a07c03e1592c
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-25 16:25:09 -0700

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,29 +1,35 @@
 #include "hash.h"
 
+// set the vector size to the default
 HashMap::HashMap()
 {
 }
 
+// delete the customer account in each slot in the vector by deleting each node in the linked list
 HashMap::~HashMap()
 {
 }
 
 // call some other premade hashFunction
 // to hash the id
+// returns the index for the hash
 int HashMap::hashFunction(Account account)
 {
 	
 }
 
+//using the hash index, insert key and value into the hashed vector
 void HashMap::insert(int hashIndex, Account account)
 {
 }
 
+//returns a pointer because the customer object needs to be edited
 Account* HashMap::search(int accountID)
 {
 	return nullptr;
 }
 
+//deletes the object with the given key from the hash
 Account HashMap::remove(int accountID)
 {
 	// if empty list
