016a55ba178045d72597b8acb4b3d2356c8d4add
MarcellaDoan <134645588+MarcellaDoan@users.noreply.github.com>
2023-05-25 15:31:46 -0700

    Create comedy.cpp

---
+++ comedy.cpp
@@ -0,0 +1,98 @@
+#include "dvd.h"
+#include "bstree.h"
+#include "comedy.h"
+
+
+/*
+char type = 'D'; // Dvd is default if not changed by contructor
+	// dvd title
+	string title = "";
+	// if the command failed
+	bool failure_ = false;
+	// customer account id
+	int id = 0;
+	// how many of the movie are in stock
+	int stock = 0;
+	// the year of the movie release
+	int year = 0;
+	// the month of the movie release
+	int month = 0;
+
+	//if a classic genre dvd and the command is to make a Classic object/effect a classic object, store major actor here
+	string majorActorFirstName = "";
+
+	string majorActorLastName = "";
+
+
+*/
+
+
+	Comedy::Comedy() 
+    {
+        setGenre('F');
+    }
+
+	// passes in a command that contains all the needed data to create a Comedy object
+	Comedy::Comedy(Command command) //build the item with the data from the comand object. 
+    {
+        setTitle(command.getVector(3));
+        setDirector(command.getVector(2)); 
+        setStock(command.getVector(1));
+        setYear(command.getVector(4));
+        setGenre('F');
+
+    }
+
+	Comedy::~Comedy() 
+    {
+
+    }
+	
+	//comparison overloads - sorted by title then year
+	bool Comedy::operator==(const Comedy& rhs) const 
+    {
+        if (getTitle() == rhs.getTitle(), getDirector() == rhs.getDirector(), getStock() == rhs.getStock(), getYear() == rhs.getYear(), getGenre() == rhs.getGenre()) 
+        {
+            return true;
+        } else 
+        {
+            return false;
+        }
+    }
+
+	bool Comedy::operator!=(const Comedy& rhs) const 
+    {
+        return !(*this == rhs);
+    }
+
+	bool Comedy::operator>(const Comedy& rhs) const
+    {
+        if (getTitle() > rhs.getTitle()) 
+        {
+            return true;
+        } else if (getTitle() == rhs.getTitle(), getYear() > rhs.getYear()) 
+        {
+            return true;
+        } else 
+        {
+            return false;
+        }
+    }
+
+	bool Comedy::operator<(const Comedy& rhs) const
+    {
+        if (*this > rhs || *this == rhs) 
+        {
+            return false;
+        } else 
+        {
+            return true;
+        }
+    }
+
+	//stream overloads
+	// display all the info 
+	ostream& operator<< (ostream& stream, const Comedy& comedy) 
+    {
+        cout << comedy.getGenre() << ", " << comedy.getStock() << ", " << comedy.getTitle() << ", " << comedy.getYear();
+    }
