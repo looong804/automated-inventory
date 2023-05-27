db481cfb857246e4fb54f2a6d56ae40337625940
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-26 17:12:29 -0700

    Update hashAccounts.h

--- hashAccounts.h
+++ hashAccounts.h
@@ -10,27 +10,28 @@ class HashAccounts
 	struct NodeData
 	{
 		T* data;
-		Node* next = nullptr;
+		NodeData* next = nullptr;
 	};
+
 public:
 	// currently does nothing
 	HashAccounts();
 	// delete the customer account in each slot in the vector by deleting each node in the linked list
 	~HashAccounts();
 
-	// returns the index for the hash
+	// returns the index for the hash; Key: account ID; Value: account 
 	int hashFunction(Account* account); 
-	//using the hash index, insert key and value into the hashed vector
-	void insert(int hashIndex, int accountID, Account* account); 
-	//returns a pointer because the customer object needs to be edited
-	Account* search(int hashIndex, int index);
+	// insert key and value into the hashed vector after hasing the key
+	void insert(Account* account);
+	//returns a pointer because the account needs to be edited
+	Account* search(Account* account);
 	
 protected:
 	// number of objects in the hashTable
-	size = 0;
+	int size = 0;
 	
 private:
 	// vector holding the linked lists of accounts
-	vector<NodeData> accounts[20]; 
+	vector<NodeData<Account>> accounts[20]; 
 };
 #endif
