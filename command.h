209a6ae9d76c9e700a9cc3ce41f0992e9b57a919
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-29 16:42:08 -0700

    Update command.h

--- command.h
+++ command.h
@@ -38,21 +38,5 @@ private:
 	// all error messages to be eccountered when reading in a command
 	// error message for: command type failure - non existant
 	string error1 = "ERROR: Command type failure - non-existant command: ";
-
-	//// these errors are no longer needed
-	//// error message for: stock amount failure - no negatives
-	//string error2 = "Error: Stock amount failure - no negatives";
-	//// error message for: account id failure - no negatives, non existant
-	//string error3 = "Error: Account id failure - no negatives, non existant";
-	//// error message for: returning a movie - never borrowed
-	//string error4 = "Error: returning a movie - never borrowed";
-	//// error message for: movie not in stock - stock is 0
-	//string error5 = "Error: movie not in stock - stock is 0";
-	//// error message for: not all fields filled **** is this needed? **************
-	//string error6 = "Error: ";
-	//// error message for: movie genre - non existant
-	//string error7 = "Error: movie genre - non existant";
-	//// error message for: movie data - non existant title/year/month/actor/director
-	//string error8 = "Error: movie data - non existant title/year/month/actor/director";
 };
 #endif
