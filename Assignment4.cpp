5a599b3b80f2ee7541c29465d6cb48f0749f5b6a
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-29 12:52:20 -0700

    Add files via upload

---
+++ Assignment4.cpp
@@ -0,0 +1,69 @@
+#include "store.h"
+
+int main(int argc, char* argv[])
+{
+	cout << "This is the beginning of the testing of the inventory tracking system purposed for a movie rental store. " << endl; 
+	cout << "This system is intended to be easily modified for real world changes: " << endl;
+	cout << "This contains the test cases for the Inventory System consisting of the following classes: Store class, Account class, Genre class, DVD class, Command class" << endl;
+	cout << "To help with sequential testing and enabling the functions that help you test the others it is recommend to implement the functions in the following order that they are tested in." << endl;
+	cout << "The functions of the Inventory System are tested in the following order: " << endl;
+
+	// declaration of all objects used for testing
+	//// making transaction objects
+	////Transaction boop("O 500 Jones George");
+	////Transaction goop("O 1001 Cash Johnny");
+	////Transaction meep("O 14 billy");
+	////Transaction spoon("D 1001 0 542");
+	////Transaction fork("D 1001 1 1542");
+	////Transaction mop("D 1001 2 5442");
+	////Transaction leap("D 1001 3 7540");
+	////Transaction peep("D 1001 7 5442");
+	////Transaction seep("D 1001 6 7542");
+	////Transaction sheep("D 1001 1 1500");
+	////Transaction keep("D 1001 2 5000");
+	////Transaction beep("D 1001 3 7000");
+	////Transaction bop("D 1001 7 6543");
+	////Transaction pop("D 1001 6 7576");
+	////Transaction lop("W 1001 7 1000");
+	////Transaction loop("W 1001 6 65");
+	////Transaction shop("W 1001 0 72");
+	////Transaction shock("T 1001 7 1001 5 54");
+	////Transaction gong("W 1001 7 20000");
+	////Transaction shlong("A 1001");
+	////Transaction pong("O 3000 Simpson Sturgill");
+	////Transaction song("O 2000 Farrar Jay");
+	////Transaction poem("O 1253 Williams Hank");
+	////Transaction roam("D 1253 0 10000");
+	////Transaction toam("D 1253 1 10000");
+	////Transaction toad("D 1253 2 10000");
+	////Transaction strong("D 1253 3 10000");
+	////Transaction stone("D 1253 4 10000");
+	////Transaction lone("D 1253 5 10000");
+	////Transaction alone("D 1253 6 10000");
+	////Transaction sleep("D 1253 7 10000");
+	////Transaction pillow("F 1253 4");
+	////Transaction dog("W 1253 2 15000");
+	////Transaction cat("A 1253");
+	////Transaction bull("T 1253 6 4356 7 765");
+	////Transaction cow("T 1001 0 3000 4 500");
+	////Transaction pow("O 9876 Nelson Willie");
+	////Transaction bam("O 9876 Willis Bob");
+	////Transaction wham("T 1253 1 9876 1 10000");
+	////// negative deposit
+	////Transaction zoom("D 500 0 -100");
+
+
+	//// end of all testing
+	//cout << "This testing is now done:" << endl;
+	//cout << "This testing can be improved in the following ways:" << endl;
+	//cout << "1: Can create an Item class that is the parent of the DVD class which will allow easy adding of other items that are not dvds (ie: books, music, games, etc.)" << endl;
+	//cout << "2: can use external tools such as valgrind to check for memory leak to encourage good practice for future work, however since the program is small this realistically does not matter." << endl;
+	//cout << "3: can improve the user feedback of the first couple of test cases, I got pickier with the outputs reablity as I did this, but now I'm too lazy to fix the old test cases." << endl;
+	//cout << "4: its also not nesscessary to use hashing for everywhere it is used" << endl;
+	//cout << "All users are free to use and add to these test cases, with the condition that they continue to pass them down to future students with improvements and an updated list on what can be improved." << endl;
+	//cout << "P.S. it can always be improved!" << endl;
+	//cout << "P.P.S. it's kind of sad you read all of this go do something meangful with your life now that you don't have to make these test cases." << endl;
+	//cout << "P.P.P.S. it's super sad that I did all of this." << endl;
+
+	return 0;
+}
