3cefdce02c474bd2f0f686b9bbdfca72762a0332
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-29 16:24:06 -0700

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char* argv[])
 {
-	cout << "This is the beginning of the testing of the inventory tracking system purposed for a movie rental store. " << endl; 
-	cout << "This system is intended to be easily modified for real world changes: " << endl;
-	cout << "This contains the test cases for the Inventory System consisting of the following classes: Store class, Account class, Genre class, DVD class, Command class" << endl;
-	cout << "To help with sequential testing and enabling the functions that help you test the others it is recommend to implement the functions in the following order that they are tested in." << endl;
-	cout << "The functions of the Inventory System are tested in the following order: " << endl;
+	// cout << "This is the beginning of the testing of the inventory tracking system purposed for a movie rental store. " << endl; 
+	// cout << "This system is intended to be easily modified for real world changes: " << endl;
+	// cout << "This contains the test cases for the Inventory System consisting of the following classes: Store class, Account class, Genre class, DVD class, Command class" << endl;
+	// cout << "To help with sequential testing and enabling the functions that help you test the others it is recommend to implement the functions in the following order that they are tested in." << endl;
+	// cout << "The functions of the Inventory System are tested in the following order: " << endl;
 
 	// should probably add just a couple text files to contain the input commands to test everything
 	// create a store using given input files
@@ -16,10 +16,24 @@ int main(int argc, char* argv[])
 	Store treasureTrove(testFile1, testFile2, testFile3);
 	cout << "Finished running the test files." << endl;
 	
-	// Testing for Command class
-	// testing for BST class
-	// testing for inventory class
-	// testing for hashing classes
+	// Testing for Command class - covered a couple that should fail and others that should work 
+	Command fail0("Z 1000 D C 10 1941 Humphrey Bogart");
+	Command fail1("X");
+	Command fail2("Z, 10, Frank Capra, It's a Wonderful Life, James Steward 11 1946");
+	Command work0("I");
+	Command work1("H");
+	Command work2("B 1000 D D Steven Spielberg, Bogus Title,");
+	Command work3("R 5000 D C 3 1971 Ruth Gordon");
+	Command work4("2000 Duck Donald");
+	Command work5("D, 10, Nancy Savoca, Dogfight, 1991");
+	Command work6("F, 10, Nora Ephron, You've Got Mail, 1998")
+	Command work7("C, 10, George Cukor, The Philadelphia Story, Cary Grant 5 1940");
+	// testing for BST class - can use the items made for testing the item class here as well
+	BST tree;
+	tree.insert()
+	// testing for inventory class - can test after constructing some BSTs
+		
+	// testing for hashing classes - can test once all the command, item, account and other functions are working
 		// testing for hashAccounts
 		// testing for hashPopular
 		// testing for hashWaitlist
