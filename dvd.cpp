f4b27ab053179a6566609ca08afefb86837f7ee3
MarcellaDoan <134645588+MarcellaDoan@users.noreply.github.com>
2023-05-30 15:46:25 -0700

    Update dvd.cpp

--- dvd.cpp
+++ dvd.cpp
@@ -8,94 +8,78 @@
 
 	Dvd::Dvd()
     {
-
+        setItemType('D');
     }
 
 	// passes in a command object that contains all the needed data to create a DVD object
 	Dvd::Dvd(Command command) 
     {
-        // does nothing, not relevent for an abstract class
+        setItemType('D');
     }
 
-	Dvd::~Dvd() 
+    void Dvd::setYear(int newYear) 
     {
-
+        year = newYear;
     }
 
-	// return the director of the movie
-	string Dvd::getDirector() const 
+
+	// addition overloads (combines the stock for most DVDs, except for classic also )
+	Dvd Dvd::operator+ (const Dvd& rhs) const 
+    {
+        Dvd dvd;
+        //int temp = rhs;
+        dvd.setStock(rhs.getStock() + getStock());
+        return dvd;
+        //stock++;
+        //this.stock += rhs;
+    }
+
+	Dvd& Dvd::operator+= (const Dvd& rhs) 
+    {
+        setStock(getStock() + rhs.getStock());
+        return *this;
+    }
+
+
+    string Dvd::getDirector() const 
     {
         return director;
     }
 
-	// return the title of the DVD
-	string Dvd::getTitle() const 
-    {
-        return title;
+	void Dvd::setDirector(string newDirector) 
+    {   
+        director = newDirector;
     }
-
 	// return the genre
 	char Dvd::getGenre() const 
     {
         return genre;
     }
 
+	void Dvd::setGenre(const char newGenre) 
+    {
+        genre = newGenre;
+    }
+
 	// return the year
 	int Dvd::getYear() const 
-    {   
+    {
         return year;
     }
 
-	// return the stock of the DVD
-	int Dvd::getStock() const 
+	void Dvd::setYear(int newYear) 
     {
-        return getStock();
+        year = newYear;    
     }
 
-	// set the stock
-	void Dvd::SetStock(int amount) 
-    {
-        Item::setStock(amount);
-    }
 
-	// up the stock by one
-	void Dvd::upStock() 
-    {
-        Item::setStock(getStock() + 1);
-    }
-
-	// down the stock by one
-	void Dvd::downStock() 
-    {
-        if (getStock() > 0) 
-        {
-            Item::setStock(getStock() - 1);
-        } else 
-        {
-            cout << "Error: movie not in stock - stock is 0" << endl;
-        }
-    }
-
-	// addition overloads (combines the stock for most DVDs, except for classic also )
-	int Dvd::operator+ (const int& rhs) const 
-    {
-        //int temp = rhs;
-        return rhs + getStock();
-        //stock++;
-        //this.stock += rhs;
-    }
-
-	int Dvd::operator+= (const int& rhs) 
-    {
-        setStock(getStock() + rhs);
-    }
 
 	//stream overloads -- should this just be in the genre class or 
 	// should we just create another for the classics where you alos put in a genre parameter
 	// output the title 
 	ostream& operator<< (ostream& stream, Dvd& dvd) 
     {
-        // not relevent for an abstract class.
+        cout << dvd.getTitle() << " " << dvd.getYear() << endl;
     }
 
 	/*
