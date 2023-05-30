c62e100ad15728f363382de8b013c838099be62d
MarcellaDoan <134645588+MarcellaDoan@users.noreply.github.com>
2023-05-30 15:49:07 -0700

    Update classic.h

--- classic.h
+++ classic.h
@@ -2,9 +2,8 @@
 #define CLASSIC_H
 
 #include "dvd.h"
-#include <vector>
 
-class Classic : public DVD
+class Classic : public Dvd
 {
 public:
 	Classic();
@@ -15,18 +14,30 @@ public:
 	// return the month
 	int getMonth() const;
 
+	int setMonth(const int newMonth);
+
+	bool getMajorActor(int const index, string &actor) const; //grabs a major actor from a certain index in the actor vector
+
+	bool hasMajorActor(string actor) const;
+
+	void addMajorActor(string actor); 
+
+	int getNumberOfActors() const; //returns the number of major actors in this object (the nextActorIndex works for this)
+
 	//comparison overloads - sorted by release date then major actor
 	// a classic movie is equal if the director, title and data are the same
 	bool operator==(const Classic& rhs) const;
 	bool operator!=(const Classic& rhs) const;
-	bool operator>=(const Classic& rhs) const;
-	bool operator<=(const Classic& rhs) const;
 	bool operator>(const Classic& rhs) const;
 	bool operator<(const Classic& rhs) const;
 
 	//stream overloads
 	// display all the info 
 	friend ostream& operator<< (ostream& stream, const Classic& classic);
+
+	Classic operator+ (const Classic& rhs) const;
+    Classic& operator+= (const Classic& rhs); 
+	Classic& operator=(const Classic& rhs); 
 
 private:
 	// major actor's first name
@@ -35,9 +46,10 @@ private:
 	//string last = "";
 	// month of release for classics
 	int month = 0;
+	int nextActorIndex = 0; //the index to use when inserting a new actor
 
 	vector<string> majorActors;
 
 	//BSTree<Classic> bstree(); 
 };
-#endif //CLASSIC_H
+#endif CLASSIC_H
