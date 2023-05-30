48570ea9adf6bd9f20c9fb931cd99c17157b395f
MarcellaDoan <134645588+MarcellaDoan@users.noreply.github.com>
2023-05-30 15:58:03 -0700

    Update item.h

--- item.h
+++ item.h
@@ -2,33 +2,50 @@
 #define ITEM_H
 
 #include "command.h"
+#include <iostream>
+
+using namespace std;
+
+
+//make the cpp for this
 
 class Item 
 {
     public: 
+
         Item();
         ~Item();
 
+
+        void setItemType(char item);
+
+        char getItemType(); 
+
         // return the title of the Item
-	string getTitle() const;
+	    string getTitle() const;
 
         // set the title of the Item
-        string setTitle(string newTitle);
+        void setTitle(string newTitle);
 
         int getStock() const;
         // set the stock
-        void SetStock(int amount);
+        void setStock(int amount);
         // up the stock by one
         void upStock();
         // down the stock by one
         void downStock();
 
+        void increasePopularity();
+
         // addition overloads
-        int operator+ (const int& rhs) const;
-        int operator+= (const int& rhs); 
+        Item operator+ (const Item& rhs) const;
+        Item& operator+= (const Item& rhs); 
 
     private: 
         int stock = 0;
         string title = "";
+        char itemType;
+        int popularity = 0; 
 };
+
 #endif
