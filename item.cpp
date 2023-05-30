53793d790c4649ddf9c65e31bdff3c16d82dbb5d
MarcellaDoan <134645588+MarcellaDoan@users.noreply.github.com>
2023-05-29 19:20:24 -0700

    Create item.cpp

---
+++ item.cpp
@@ -0,0 +1,86 @@
+#ifndef ITEM_CPP
+#define ITEM_CPP
+
+#include "item.h"
+
+
+
+
+//make the cpp for this
+
+
+
+        Item::Item() 
+        {
+            setStock(0);
+            setTitle("");
+        }
+
+        void Item::setItemType(char const item) 
+        {
+            itemType = item; 
+        }
+
+        char Item::getItemType() 
+        {
+            return itemType;
+        }
+
+        // return the title of the Item
+	    string Item::getTitle() const 
+        {
+            return title;
+        }
+
+        // set the title of the Item
+        void Item::setTitle(string const newTitle) 
+        {
+            title = newTitle; 
+        }
+
+        int Item::getStock() const 
+        {   
+            return stock;
+        }
+        // set the stock
+        void Item::setStock(int const amount) 
+        {
+            stock = amount;
+        }
+        // up the stock by one
+        void Item::upStock() 
+        {   
+            stock++;
+        }
+        // down the stock by one
+        void Item::downStock() 
+        {
+             if (stock > 0) 
+            {
+                stock--;
+            } else 
+            {
+                cout << "Error: movie not in stock - stock is 0" << endl;
+            }
+        }
+
+        // addition overloads
+        Item Item::operator+ (const Item& rhs) const 
+        {
+            Item item;
+
+            //int hi = item.getStock();
+
+            item.setStock(rhs.getStock() + stock);
+            return item;
+            //return rhs.getStock() + stock;
+        }
+
+        Item& Item::operator+= (const Item& rhs) 
+        {
+            stock += rhs.getStock();
+            return *this;
+        }
+
+
+#endif
