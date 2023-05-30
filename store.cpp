8614a1ad34526af4ec2aad54406a0a1c5925b94c
looong804 <77555150+looong804@users.noreply.github.com>
2023-05-30 16:32:04 -0700

    Add files via upload

--- store.cpp
+++ store.cpp
@@ -3,6 +3,8 @@
 
 #include "store.h"
 #include <fstream>
+#include <sstream>
+#include <string>
 
 //constructors
 //default -- unused at the moment
@@ -17,7 +19,7 @@ Store::Store(vector<string> files){
 }//close files constructor
 
 //destructor
-Store::~Store() {
+Store::~Store(){
 
 }//close destructor
 
@@ -26,126 +28,219 @@ Store::~Store() {
 //Setters
 //
 void Store::setStock(){
+    //
+    while(!stock.empty()){
+        //Retrieve first item
+        Command temp = stock.at(0);
 
+        if(temp.getVector(0) == "F"){
+            Item newItem;
+
+            if(newItem.getTitle() == comedyTree.search(newItem).getTitle()){
+                newItem.SetStock(comedyTree.search(newItem).getStock() + temp.getVector(1));
+            }else{
+                newItem.setTitle(temp.getVector(3));
+                newItem.SetStock(stoi(temp.getVector(1)));
+                inventory().getVector()[0]; //TODO: insert in right index
+            }
+        }else if(temp.getVector(0) == "D"){
+            Item newItem;
+
+            if(newItem.getTitle() == dramaTree.search(newItem).getTitle()){
+                newItem.SetStock(dramaTree.search(newItem).getStock() + temp.getVector(1));
+            }else{
+                newItem.setTitle(temp.getVector(3));
+                newItem.SetStock(stoi(temp.getVector(1)));
+                inventory().getVector()[0]; //TODO: insert in right index
+            }
+        }else if(temp.getVector(0) == "C"){
+            Item newItem;
+
+            if(newItem.getTitle() == classicTree.search(newItem).getTitle()){
+                newItem.SetStock(classicTree.search(newItem).getStock() + temp.getVector(1));
+            }else{
+                newItem.setTitle(temp.getVector(3));
+                newItem.SetStock(stoi(temp.getVector(1)));
+                inventory().getVector()[0]; //TODO: insert in right index
+            }
+        }
+
+        stock.erase(stock.begin());
+    }
 }//close setStock
 
 //
 void Store::setAccounts() {
+    while(!customers.empty()){
+        Command temp = customers.at(0);
 
+        if(temp.getVector(0) == "K"){
+            Customer newCustomer;
+
+            if(!isAccount(newCustomer)){
+                newCustomer.setID(stoi(temp.spaceParser(0)[0])); //TODO: check
+                newCustomer.setLast(temp.spaceParser(1)[1]); //TODO: check
+                newCustomer.setFirst(temp.spaceParser(2)[2]); //TODO: check
+
+                insert(newCustomer);
+            }
+        }
+
+        customers.erase(customers.begin());
+    }
 }//close setAccounts
 
 //
-void Store::executeCommands() {
+void Store::executeActions() {
+    while(!actions.empty()){
+        Command temp = actions.at(0);
 
+        if(temp.getVector(0) == "B"){
+            borrowItem(temp);
+        }else if(temp.getVector(0) == "R"){
+            returnItem(temp);
+        }else if(temp.getVector(0) == "I"){
+            //list inventory
+        }else if(temp.getVector(0) == "H"){
+            accountHistory(stoi(temp.getVector(1)));
+        }
+
+        actions.erase(actions.begin());
+    }
 }//close executeCommands
+
+
+//Getters
+//
+vector<char> Store::getCommandTypes(){
+    return this -> commandTypes;
+}//close getCommand
+
+// return number of accounts
+int Store::numAccounts() const{
+    //return hash account's size
+    return 0;
+}//close numCustomers
+
 
 
 //Functions
 //read all files in the files vector
 void Store::readFiles(){
-    int index = 0;
-
     //iterate through file names
-    //TODO: check if names should be in order; assumes files are in order
-    while(index != filesNames.size()){
-        //items file
-        if(index == 0){
-            ifstream itemFile(filesNames[index]);
+    for(auto &filesName : filesNames){
+        ifstream file(filesName);
 
-            //insert items
-            while(!itemFile.eof()){
-                string line;
+        //insert items
+        while(!file.eof()){
+            string line;
 
-                while(getline(itemFile, line)){
-                    stock.push_back(Command(line));
-                }//close insert items
-            }//close last line check
+            while(getline(file, line)){
+                Command tempCom(line);
+                string letter = tempCom.getVector().at(0);
 
-            index++;
-            continue;
-        }//close items file check
-
-        //accounts file
-        if(index == 1){
-            ifstream accountFile(filesNames[index]);
-
-            //insert accounts
-            while(!accountFile.eof()){
-                string line;
-
-                while(getline(accountFile,line)){
-                    customers.push_back(Command(line));
-                }//close insert accounts
-            }//close last line check
-
-            index++;
-            continue;
-        }//close accounts file check
-
-        //commands file
-        if(index == 2){
-            ifstream commandFile(filesNames[index]);
-
-            //insert commands
-            while(!commandFile.eof()){
-                string line;
-
-                while(getline(commandFile,line)){
-                    commands.push_back(Command(line));
-                }//close insert commands
-            }//close last line check
-
-            index++;
-            continue;
-        }//close commands file check
+                //checks if this is a movie command
+                if(letter == "F" || letter == "D" || letter == "C"){
+                    stock.push_back(tempCom);
+                //checks if this is a customer command
+                }else if(letter == "K"){
+                    customers.push_back(tempCom);
+                //checks if this is a command
+                }else if(letter == "B" || letter == "R"
+                    || letter == "I" || letter == "H"){
+                    actions.push_back(tempCom);
+                //not a recognized command
+                }//close command check
+            }//close insert items
+        }//close last line check
     }//close num files check
 }//close files
 
 // open an account
 bool Store::openAccount(Command action){
-    return false;
+    Customer newAcct(action);
+
+    //if account already exists
+    if(customers[0] == isAccount(newAcct))
+        return false;
+
+    //insert account into hashtable, providing ID as key
+    insert(newAcct);
+
+    return true;
 }//close openAccount
 
 //
-Customer* Store::findAccount(int id) const{
+Account* Store::findAccount(int id) const{
+    //calls hashAccount's search function
+    
     return nullptr;
 }//close findAccount
 
 //
 DVD* Store::findDrama(Drama& dvd) const{
+    //checks that the first index is D
+    //parse through binary tree for dvd
+    //search via director then title
     return nullptr;
 }//close findDrama
 
 //
 DVD* Store::findComedy(Comedy& dvd) const{
+    //checks that the first index is F
+    //parse through binary tree for dvd
+    //search via title then year released
     return nullptr;
 }//close findComedy
 
-// find a classic dvd and verify that it exsits
+// find a classic dvd and verify that it exists
 DVD* Store::findClassic(Classic& dvd) const{
+    //checks the first index is C
+    //parse through binary tree for dvd
+    //search via release date then major actor
+
     return nullptr;
 }//close findClassic
 
 
 // return a borrowed item
 bool Store::returnItem(Command action){
+    //checks if item exists
+    //checks if Customer has the item checked out
+    //add to history that customer returned item
+    //checks if there is a waitlist for item
+        //if customer is waiting for item, check out item
+        //directly to waiting customer
+        //add to that customer's history
+        //remove customer from waitlist
+        //do not change stock or request counter
+        //if there is no waitlist, return item to shelf
+        //increase stock
+
     return false;
 }//close returnItem
 
 // borrow an item
 bool Store::borrowItem(Command action){
+    //checks if the Item exists
+    //checks if Customer has already borrowed this Item
+    //checks if the Item is available
+        //if available, checks item out to Customer
+        //add to history that customer borrowed item
+        //decrement stock
+        //increment request counter
+        //if not available, place customer in waitlist
+        //consider handing item to customer in waitlist
+        //instead of returning to inventory
     return false;
 }//close borrowItem
 
 // print out account history given the id number
 void Store::accountHistory(int id) const{
-
+    //use id to find account in hash table
+    //retrieve account
+    //use account showHistory
 }//close accountHistory
-
-// return number of customer accounts
-int Store::numCustomers() const{
-    return 0;
-}//close numCustomers
 
 
 
--- store.h
+++ store.h
@@ -1,52 +1,90 @@
 #ifndef STORE_H
 #define STORE_H
 
+#include "account.h"
 #include "classic.h"
+#include "command.h"
 #include "comedy.h"
 #include "drama.h"
-#include "hashAccounts.h"
-#include "hashWaitlist.h"
-#include "hashPopular.h"
 #include "inventory.h"
 #include "item.h"
-#include "command.h"
-#include "account.h"
+#include "hashAccounts.h"
+#include "hashPopular.h"
+#include "hashWaitlist.h"
+
+using namespace std;
+
+// - could create a chain class that holds stores and has the num of stores, and 
+// you can create a chain by reading in a folder that contains a folder for 
+// each store that has the three files needed to make a store
+// - could create an item class to be the parent of the dvd class, 
+// which enables quick adds of new items in stock
+// - could add an accounts class to be the parent class of all account type classes
+// such as customer account, IT account, employee account, manager account
+// - could create another level of categorizing with classes like video, audio, games
+// these would then be parent classes of things like; For video: tvs, cables, movies, players
+// For Audio: speakers, music, players, For games: video games, controls, game systems
+// 
+// - could add a time to return things by and for each day after the return time charge more
+// - could add a charge system/ tab system or make customers store money on their account
+// - could add prices to all the items in the inventory
+// - could add discounting functions that will discount all the prices of items 
+// depending on their categories and types, and after a certain time return them to full price
 
 class Store
 {
 public:
+    // Constructor
 	// not needed
 	Store();
 	// takes in 3 files with inventory data, account data, and command data to make the store
 	// calls all 3 read file functions
 	Store(vector<string> files);
-	// I think this won't have to do anything if all other functions and destructors are made right
+
+    //Destructor
+    // I think this won't have to do anything if all other functions and destructors are made right
 	~Store();
 
-	//actions
+	//Getters
+    //retrieve the command types
+    vector<char> getCommandTypes();
+    // return number of customer accounts
+    int numAccounts() const;
+
+    //Setters
 	// reads everything in stock vector and inserts the items
 	// make sure to delete each object in the vector after reading/executing
 	void setStock(); 
 	//reads everything in stock vector and inserts the accounts
 	// make sure to delete each object in the vector after reading/executing
 	void setAccounts(); 
-	//reads everything in command vector and exceutes the commands
+	//reads everything in command vector and executes the commands
 	// if the commands are borrows or returns put into customer history else delete them after reading/executing
-	void executeCommands(); 
-	// find customer account and verify it exsits
-	Customer* findAccount(int id) const;
-	// find a drama dvd and verify that it exsits
+	void executeActions();
+
+    //Creation Functions
+    // open an account
+    bool openAccount(Command action);
+    // take in a vector to read all files in the vector
+    void readFiles();
+
+    //Search Functions
+	// find customer account and verify it exists
+	Account* findAccount(int id) const;
+	// find a drama dvd and verify that it exists
 	DVD* findDrama(Drama& dvd) const;
-	// find a comedy dvd and verify that it exsits
+	// find a comedy dvd and verify that it exists
 	DVD* findComedy(Comedy& dvd) const;
-	// find a classic dvd and verify that it exsits
+	// find a classic dvd and verify that it exists
 	DVD* findClassic(Classic& dvd) const;
-	// open an account
-	bool openAccount(Command action);
-	// take in a vector to read all files in the vector
-	void readFiles();
-	// return number of customer accounts
-	int numCustomers() const;
+
+    //Command Functions
+	// return a borrowed item
+	bool returnItem(Command action);
+	// borrow an item
+	bool borrowItem(Command action);
+	// print out account history given the id number
+	void accountHistory(int id) const;
 
 	//stream overloads
 	// prints out the inventory object using <<
@@ -57,22 +95,23 @@ private:
 	vector<Command> stock; 
 	// parsed info of the customers to be added
 	vector<Command> customers; 
-	// parsed info of the commands to be executed (borrow, return, etc.)
-	vector<Command> actions; 
+	// parsed info of the actions to be executed (borrow, return, etc.)
+	vector<Command> actions;
 	// contains all the customer accounts
 	HashAccounts accounts;
-	// contains all the most requested items
-	HashPopular popularItems;
-	// contains all the borrow commands that failed due to no avaliable stock
-	HashWaitlist waitlist;
+    // contains top ten most requested items
+    HashPopular popularItems;
+    // contains all the borrow requests placed on hold due to unavailable stock
+    HashWaitlist waitlist;
 	// contains all the items
 	Inventory inventory();
 	// total number of all items
-	int totalStock = 0;
+    int totalStock = 0;
 	// vector of strings storing all the file names
 	vector<string> filesNames;
 	// chars for their indicated command type
-	char createComedy = 'F';
+
+    char createComedy = 'F';
 	char createDrama = 'D';
 	char createClassic = 'C';
 	char showInventory = 'I';
@@ -80,7 +119,8 @@ private:
 	char checkoutItem = 'B';
 	char givebackItem = 'R';
 	// vector that stores all accepted command types
-	vector<char> commandTypes {createComedy, createDrama, createClassic, 
+	vector<char> commandTypes {createComedy, createDrama, createClassic,
 		showInventory, showCustomerHistory, checkoutItem, givebackItem};
 };
+
 #endif
