34315ca30a4969c46173e3127bb79512a8cda4d2
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-30 16:48:29 -0700

    Update hashWaitlist.h

--- hashWaitlist.h
+++ hashWaitlist.h
@@ -4,25 +4,24 @@
 #include "hashAccounts.h"
 
 // this class uses hash chaining
-class HashWaitlist : public HashAccounts
+class HashWaitlist : public Hash
 {
-
 public:
 	// currently does nothing
 	HashWaitlist();
 	// delete the command object in each slot in the vector by deleting each node in the linked list
 	~HashWaitlist();
 
-	// returns the index for the hash; Key: item title; Value: command
-	int hashFunction(Command* command);
 	// insert key and value into the hashed vector after hasing the key
 	void insert(Command* command);
 	// check the waitlist using the hashIndex for the item title
 	Command* search(Command* command);
 	// using the value, hash the key stored in the value, then search the given slot for the value 
-	void remove(Command* command);
+	Command remove(Command* command);
 
 private:
+	// returns the index for the hash; Key: item title; Value: command
+	int hashFunction(Command* command);
 	// vector holding the failed borrow commands
 	vector<NodeData<Command>> waitlist[25];
 };
