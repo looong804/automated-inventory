223490237d92dde24980357ee85c66a963790572
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-30 16:47:11 -0700

    Update hashPopular.h

--- hashPopular.h
+++ hashPopular.h
@@ -2,31 +2,33 @@
 #define HASHPOP_H
 
 #include "hashAccounts.h"
+#include "item.h"
 
 // this class uses open addressing and stores a pointer to the item
-class HashPopular : public HashAccounts
+class HashPopular : public Hash
 {
-
 public:
 	// currently does nothing
 	HashPopular();
 	// set all the pointers in each slot in the vector to nullptr
 	~HashPopular();
 
-	// returns the index for the hash; Key: item title; Value: item 
-	int hashFunction(Item* item);
 	// insert key and value into the hashed vector after hashing the title
 	void insert(Item* item);
 	// check the popular vector for the item using the hashIndex for the item title
 	Item* search(Item* item);
 	// won't work using hashing but you can just use linear searching won't be bad since its only 10 slots
 	void swap(Item* newitem, Item* olditem);
-
-protected:
-	int size = 10;
-
+	// check if all slots are taken
+	bool isFull();
+	
 private:
+	// returns the index for the hash; Key: item title; Value: item 
+	int hashFunction(Item* item);
+
 	// vector holding the failed borrow commands
 	vector<Item*> popular[10];
+	// check the popular vector for the item and return its index if found
+	int searchForSwap(Item* item);
 };
 #endif
