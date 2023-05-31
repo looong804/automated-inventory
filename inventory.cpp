0090c7e6eb9ffd4fa6ccda2aa1626074dd45ec84
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-31 11:02:21 -0700

    Update inventory.cpp

--- inventory.cpp
+++ inventory.cpp
@@ -10,6 +10,14 @@ Inventory::~Inventory()
 {
 }
 
+// returns the roots to their respective trees
+Inventory::Node<Classic>* getClassicTree()
+{
+	return classicTree.getRoot();	
+}
+Inventory::Node<Comedy>* getComedyTree();
+Inventory::Node<Drama>* getDramaTree();
+
 // output the whole inventory by going through the vector
 // print out the comedy, then drama, then classic
 // the above order is how they are stored in the vector
