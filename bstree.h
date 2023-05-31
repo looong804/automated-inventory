2df152781a6c0da3fe034fe32b1d521cbaf4d5aa
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-05-31 11:14:18 -0700

    Update bstree.h

--- bstree.h
+++ bstree.h
@@ -62,13 +62,13 @@ void BSTree::inOrderTraversal(Node<T>* look) const
 		// call inorder again but going to the left
 		inOrderTraversal(look->left);
 		// print out the left node movie
-		cout << (look->left->movie) << endl;
+		cout << (look->left->item) << endl;
 		// print out the current node movie
-		cout << (look->movie) << endl;
+		cout << (look->item) << endl;
 		// call inorder for the right
 		inOrderTraversal(look->right);
 		// print the movie from the right node
-		cout << (look->right->movie) << endl;
+		cout << (look->right->item) << endl;
 	}
 }
 
@@ -85,13 +85,13 @@ void BSTree::insert(M* item)
 	{
 //		cout << "tree is emtpy so set root to passed in item" << endl;
 		// set root movie to item
-		root->movie = item;
+		root->item = item;
 		//increase the tree size
 		size++;
 		return;
 	}
 	// if equal to the root
-	if (root->movie == item)
+	if (root->item == item)
 	{
 //			cerr << "duplicate item" << endl;
 		return;
@@ -104,14 +104,14 @@ void BSTree::insert(M* item)
 //	cerr << "what is p_node->next->data1: " << *p_node->next->data << endl;
 //	cerr << "what is data1: " << *data << endl;
 	// while passed item is greater than traveling pointer's item move right
-	while ((item > p_node->movie) && ((p_node->right) != nullptr))
+	while ((item > p_node->item) && ((p_node->right) != nullptr))
 	{
 //		cout << "inside the move right while loop" << endl;
 		p_node = p_node->right;
 //		cerr << "where is p_node3: " << *p_node->data << endl; 
 	}
 	// while passed item is smaller than traveling pointer's item move right
-	while ((item < p_node->movie) && ((p_node->left) != nullptr))
+	while ((item < p_node->item) && ((p_node->left) != nullptr))
 	{
 //		cout << "inside the move left while loop" << endl;
 		p_node = p_node->left;
@@ -119,13 +119,13 @@ void BSTree::insert(M* item)
 	}
 	// placing new node in tree
 	// if passed in item is greater
-	if ((item > p_node->movie) && (p_node->right) == nullptr)
+	if ((item > p_node->item) && (p_node->right) == nullptr)
 	{
 //		cout << "the item is greater than the current node put in right" << endl;
 		// create new node
 		Node<T>* insert_node = new Node;
 		// set item pointer in new node to passed in item
-		(insert_node->movie) = item;
+		(insert_node->item) = item;
 		// have p_node's right point to the new node
 		p_node->right = insert_node;
 		//increase the tree size
@@ -133,13 +133,13 @@ void BSTree::insert(M* item)
 		return;
 	}
 	// if passed in item is less than
-	else if ((item < p_node->movie) && (p_node->left) == nullptr)
+	else if ((item < p_node->item) && (p_node->left) == nullptr)
 	{
 		//		cout << "the id is smaller than the current node put in the left" << endl;
 		// create new node
 		Node<T>* insert_node = new Node;
 		// set item pointer in new node to passed in item
-		(insert_node->movie) = item;
+		(insert_node->item) = item;
 		// have p_node's right point to the new node
 		p_node->left = insert_node;
 		//increase the tree size
@@ -154,7 +154,7 @@ void BSTree::insert(M* item)
 template<class M>
 Node<T>* BSTree::search(M* target) const
 {
-	//cout << root->movie;
+	//cout << root->item;
 	// if empty tree
 	if (root == nullptr)
 	{
@@ -162,7 +162,7 @@ Node<T>* BSTree::search(M* target) const
 		return nullptr;
 	}
 	// first in tree
-	if (root->movie == target)
+	if (root->item == target)
 	{
 //		cout << "is the root " << endl;
 		Node<T>* found = root;
@@ -171,24 +171,24 @@ Node<T>* BSTree::search(M* target) const
 	// traverse the rest of the tree
 //	cout << "going through the rest of the tree " << endl;
 	Node<T>* p_node = root;
-//	cout << p_node->movie << endl;
+//	cout << p_node->item << endl;
 //	cout << target << endl;
 	// while passed item is greater than traveling pointer's item move right
-	while (((target) > p_node->movie) && ((p_node->right) != nullptr))
+	while (((target) > p_node->item) && ((p_node->right) != nullptr))
 	{
 //		cout << "in the moving right while loop in the retrieve function" << endl;
 		p_node = p_node->right;
-//		cerr << "where is p_node3: " << *p_node->movie << endl; 
+//		cerr << "where is p_node3: " << *p_node->item << endl; 
 	}
 	// while passed item is smaller than traveling pointer's item move right
-	while ((target < p_node->movie) && ((p_node->left) != nullptr))
+	while ((target < p_node->item) && ((p_node->left) != nullptr))
 	{
 //		cout << "in the moving left while loop in the retrieve function" << endl;
 		p_node = p_node->left;
-//		cerr << "where is p_node3: " << *p_node->data << endl; 
+//		cerr << "where is p_node3: " << *p_node->item << endl; 
 	}
 	// if target is found on the left
-	if (((p_node->left) != nullptr) && target == p_node->left->movie)
+	if (((p_node->left) != nullptr) && target == p_node->left->item)
 	{
 		Node<T>* found = (p_node->left);
 		// set p_node pointer to nullptr
@@ -197,7 +197,7 @@ Node<T>* BSTree::search(M* target) const
 		return found;
 	}
 	// if the target is found on the right
-	else if (((p_node->right) != nullptr) && target == p_node->right->movie)
+	else if (((p_node->right) != nullptr) && target == p_node->right->item)
 	{
 		Node<T>* found = (p_node->right);
 		// set p_node pointer to nullptr
@@ -206,7 +206,7 @@ Node<T>* BSTree::search(M* target) const
 		return found;
 	}
 	// if the target is found on current node -- if working right should never get here
-	else if (p_node->movie == target)
+	else if (p_node->item == target)
 	{
 		Node<T>* found = (p_node);
 		// set p_node pointer to nullptr
