39afc177b90d1b854bc88cfe3c70d66cc0c1d463
looong804 <77555150+looong804@users.noreply.github.com>
2023-06-01 09:54:44 -0700

    Add files via upload

---
+++ customer.cpp
@@ -0,0 +1,125 @@
+#ifndef CUSTOMER_CPP
+#define CUSTOMER_CPP
+
+#include "customer.h"
+
+//
+Customer::Customer(){
+
+}//close default constructor
+
+//
+Customer::Customer(Command createCustomerAccount){
+    stringstream strstrm(createCustomerAccount.getVector(1)); //retrieves account info
+    string tempInfo;
+
+    strstrm >> tempInfo;
+    this -> setID(stoi(tempInfo));
+    strstrm >> tempInfo;
+    this -> setLast(tempInfo);
+    strstrm >> tempInfo;
+    this -> setFirst(tempInfo);
+}//close create customer constructor
+
+
+
+//
+Customer::~Customer(){
+
+}//close destructor
+
+
+
+//Getters
+vector<Item> Customer::getItemsOut()
+{
+    return itemsOut;
+}//close getItemsOut
+
+//
+string Customer::getFirst() const{
+    return Account::getFirst();
+}//close getFirst
+
+//
+string Customer::getLast() const{
+    return Account::getLast();
+}//close getLast
+
+//
+string Customer::getFullName() const{
+    return Account::getFullName();
+}//close getFullName
+
+//
+int Customer::getID() const{
+    return Account::getID();
+}//close getID
+
+
+
+//Functions
+//
+void Customer::showHistory(){
+    //print out oldest transaction to most recent
+
+    //parse through vector history
+    for(int i = 0; i < history.size(); i++){
+        string transaction;
+
+        for(int j = 0; j < history[i].getVector().size(); j++){
+            transaction += history[i].getVector(j) + " ";
+        }
+
+        cout << transaction << endl;
+    }
+}//close showHistory
+
+//
+//Pseudocode
+//if available, checks item out to Customer
+//add to history that customer borrowed item
+void Customer::borrow(Item item, Command action){
+    itemsOut.push_back(item);
+    history.push_back(action);
+}//close borrow
+
+//
+//Pseudocode
+//checks if item exists
+//checks if Customer has the item checked out
+//add to history that customer returned item
+//remove from itemsOut
+void Customer::returnItem(Item item, Command action){
+    for(auto it = itemsOut.begin(); it < itemsOut.end(); it++){
+        if(item == *it){
+            itemsOut.erase(it);
+        }
+    }
+
+    history.push_back(action);
+}//close returnItem
+
+
+
+//Overload operator
+//
+ostream& operator<<(ostream& out, const Customer& cus){
+    return out << cus.getID() << cus.getLast() << cus.getFirst();
+}//close operator<<
+
+//
+bool Customer::operator==(const Customer& cus){
+    return this -> getID() == cus.getID() &&
+        this -> getLast() == cus.getLast() &&
+        this -> getFirst() == cus.getFirst();
+}//close operator==
+
+//
+bool Customer::operator!=(const Customer &cus){
+    return this -> getID() != cus.getID() ||
+        this -> getLast() != cus.getLast() ||
+        this -> getFirst() != cus.getFirst();
+}//close operator!=
+
+#endif //CUSTOMER_CPP
--- customer.h
+++ customer.h
@@ -3,32 +3,39 @@
 
 #include "item.h"
 #include <vector>
+#include <sstream>
 #include "account.h"
 
-class Customer : public Account
-{
+class Customer : public Account{
 public:
 	// not needed
 	Customer();
 	// used when reading in the account file
-	Customer(Command createCustomerAccount);
+	explicit Customer(Command createCustomerAccount);
 	// deletes the command items in the history vector
 	~Customer();
 
+    //Getters
+    vector<Item> getItemsOut();
+    string getFirst() const;
+    string getLast() const;
+    string getFullName() const;
+    int getID() const;
+
 	//Functions
 	void showHistory(); //prints the customer's history
-
-	void borrow(Item item);
-
-	void returnItem(Item item);
+	void borrow(Item item, Command action);
+	void returnItem(Item item, Command action);
 
 
 	//stream overloads
 	// display the customer history
-	friend ostream& operator<< (ostream& stream, const Customer& cus);
-
+	friend ostream& operator<< (ostream& out, const Customer& cus);
+    bool operator==(const Customer& cus);
+    bool operator!=(const Customer& cus);
 private:
 	// holds the account history
-	vector<Command> commands;
+	vector<Command> history;
+    vector<Item> itemsOut;
 };
 #endif //CUSTOMER_H
