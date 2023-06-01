acbb11a37a8c0269b3de70a9d4fb928a443f53fb
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-06-01 13:25:04 -0700

    Update store.cpp

--- store.cpp
+++ store.cpp
@@ -37,35 +37,35 @@ void Store::setStock(){
         Command temp = stock.at(0);
 
         if(temp.getVector(0) == "F"){
-            Comedy newComedy;
+            Item newItem;
 
-            if(newComedy.getTitle() == inventory.comedyTree.search(newComedy).getTitle()){
-                newComedy.setStock(inventory.comedyTree.search(newComedy).getStock() + temp.getVector(1));
+            if(newItem.getTitle() == inventory.comedyTree.search(newItem).getTitle()){
+                newItem.setStock(inventory.comedyTree.search(newItem).getStock() + temp.getVector(1));
             }else{
-                newComedy.setTitle(temp.getVector(3));
-                newComedy.setStock(stoi(temp.getVector(1)));
+                newItem.setTitle(temp.getVector(3));
+                newItem.setStock(stoi(temp.getVector(1)));
                 inventory.getInventory()[0].insert(); //TODO: insert in right index
                 increaseTotal();
             }
         }else if(temp.getVector(0) == "D"){
-            Drama newDrama;
+            Item newItem;
 
-            if(newDrama.getTitle() == inventory.dramaTree.search(newDrama).getTitle()){
-                newDrama.setStock(inventory.dramaTree.search(newDrama).getStock() + temp.getVector(1));
+            if(newItem.getTitle() == inventory.dramaTree.search(newItem).getTitle()){
+                newItem.setStock(inventory.dramaTree.search(newItem).getStock() + temp.getVector(1));
             }else{
-                newDrama.setTitle(temp.getVector(3));
-                newDrama.setStock(stoi(temp.getVector(1)));
+                newItem.setTitle(temp.getVector(3));
+                newItem.setStock(stoi(temp.getVector(1)));
                 inventory.getInventory()[1].insert(); //TODO: insert in right index
                 increaseTotal();
             }
         }else if(temp.getVector(0) == "C"){
-            Classic newClassic;
+            Item newItem;
 
-            if(newClassic.getTitle() == inventory.classicTree.search(newClassic).getTitle()){
-                newClassic.setStock(inventory.classicTree.search(newClassic).getStock() + temp.getVector(1));
+            if(newItem.getTitle() == inventory.classicTree.search(newItem).getTitle()){
+                newItem.setStock(inventory.classicTree.search(newItem).getStock() + temp.getVector(1));
             }else{
-                newClassic.setTitle(temp.getVector(3));
-                newClassic.setStock(stoi(temp.getVector(1)));
+                newItem.setTitle(temp.getVector(3));
+                newItem.setStock(stoi(temp.getVector(1)));
                 inventory.getInventory()[2].insert(); //TODO: insert in the right index
                 increaseTotal();
             }
@@ -123,11 +123,6 @@ void Store::increaseTotal(){
 
 
 //Getters
-//
-vector<char> Store::getCommandTypes(){
-    return this -> commandTypes;
-}//close getCommand
-
 // return number of accounts
 int Store::numAccounts() const{
     return accounts.getSize(); //return hash account's size
