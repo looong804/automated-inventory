aaa75867b9f9fe07ed0589772e540175272318cb
Steven Wenzel <45366232+StevenWenzel32@users.noreply.github.com>
2023-06-01 22:22:45 -0700

    Update hashWaitlist.cpp

--- hashWaitlist.cpp
+++ hashWaitlist.cpp
@@ -10,7 +10,7 @@ HashWaitlist::~HashWaitlist()
 {
 }
 
-void HashWaitlist::insert(Command* command)
+void HashWaitlist::insert(Commands* command)
 {
 	// check if the command pointer is empty
 	if (command != nullptr)
@@ -27,9 +27,9 @@ void HashWaitlist::insert(Command* command)
 		else
 		{
 			// create a nodedata equal to the first node in the linked list
-			NodeData<Command> start = waitlist->at(hashIndex);
+			NodeData<Commands> start = waitlist->at(hashIndex);
 			// create a pointer to traverse the linked list starting at the 2 node
-			NodeData<Command>* temp = start.next;
+			NodeData<Commands>* temp = start.next;
 
 			// if temp is not empty
 			if (temp != nullptr)
@@ -41,7 +41,7 @@ void HashWaitlist::insert(Command* command)
 					temp = temp->next;
 				}
 				// add in a new node after temp
-				temp->next = new NodeData<Command>;
+				temp->next = new NodeData<Commands>;
 				// set the new node data to passed command
 				temp->next->data = command;
 				// set the new node's next pointer to nullptr
@@ -51,7 +51,7 @@ void HashWaitlist::insert(Command* command)
 			else
 			{
 				// add in a new node to temps current location
-				start.next = new NodeData<Command>;
+				start.next = new NodeData<Commands>;
 				// set the new node data to passed command
 				start.next->data = command;
 				// set the new node's next pointer to nullptr
@@ -63,7 +63,7 @@ void HashWaitlist::insert(Command* command)
 	}
 }
 
-Command* HashWaitlist::search(Command* command)
+Commands* HashWaitlist::search(Commands* command)
 {
 	// check if the command pointer is empty
 	if (command == nullptr)
@@ -72,7 +72,7 @@ Command* HashWaitlist::search(Command* command)
 	}
 
 	// nodeData equal to the first node in the linked list 
-	NodeData<Command> start = waitlist->at(hashFunction(command));
+	NodeData<Commands> start = waitlist->at(hashFunction(command));
 
 	// check if its the first node
 	if (start.data == command)
@@ -83,7 +83,7 @@ Command* HashWaitlist::search(Command* command)
 	else
 	{
 		// nodeData pointer to traverse the list starting at the second node
-		NodeData<Command>* looky = start.next;
+		NodeData<Commands>* looky = start.next;
 
 		// while the account isn't found
 		while (true)
@@ -107,7 +107,7 @@ Command* HashWaitlist::search(Command* command)
 }
 
 // return a pointer to customer
-int HashWaitlist::searchGiveCustomer(Command* command)
+int HashWaitlist::searchGiveCustomer(Commands* command)
 {
 	// check if the command pointer is empty
 	if (command == nullptr)
@@ -116,7 +116,7 @@ int HashWaitlist::searchGiveCustomer(Command* command)
 	}
 
 	// nodeData equal to the first node in the linked list 
-	NodeData<Command> start = waitlist->at(hashFunction(command));
+	NodeData<Commands> start = waitlist->at(hashFunction(command));
 
 	// check if its the first node
 	if (start.data == command)
@@ -131,7 +131,7 @@ int HashWaitlist::searchGiveCustomer(Command* command)
 	else
 	{
 		// nodeData pointer to traverse the list starting at the second node
-		NodeData<Command>* looky = start.next;
+		NodeData<Commands>* looky = start.next;
 
 		// while the account isn't found
 		while (true)
@@ -158,7 +158,7 @@ int HashWaitlist::searchGiveCustomer(Command* command)
 }
 
 // remove the command from the linked list
-int HashWaitlist::remove(Command* command)
+int HashWaitlist::remove(Commands* command)
 {
 	// check if the command pointer is empty
 	if (command != nullptr)
@@ -169,16 +169,16 @@ int HashWaitlist::remove(Command* command)
 			// find the hashindex
 			int hashIndex = hashFunction(command);
 			// create a nodedata equal to the first node in the linked list
-			NodeData<Command> start = waitlist->at(hashIndex);
+			NodeData<Commands> start = waitlist->at(hashIndex);
 			// create a pointer to traverse the linked list starting at the 2 node
-			NodeData<Command>* temp = start.next;
+			NodeData<Commands>* temp = start.next;
 
 			// if the command is at the start
 			if (command == start.data)
 			{
 				// get the id from the command
 				int id = stoi(command->spaceParser(1).at(0));
-				
+
 				// insert start's next into the slot in the vector
 				waitlist->at(hashIndex) = *start.next;
 				// delete the start data
@@ -187,7 +187,7 @@ int HashWaitlist::remove(Command* command)
 				start.data = nullptr;
 				// set the start next to nullptr
 				start.next = nullptr;
-				
+
 				// search for the account using the id
 				// and return the pointer to the account
 				return id;
@@ -200,7 +200,7 @@ int HashWaitlist::remove(Command* command)
 				{
 					// get the id from the command
 					int id = stoi(command->spaceParser(1).at(0));
-					
+
 					// have start point to temps next
 					start.next = temp->next;
 					// delete the temp data
@@ -209,7 +209,7 @@ int HashWaitlist::remove(Command* command)
 					temp->data = nullptr;
 					// set temps next to nullptr
 					temp->next = nullptr;
-					
+
 					// search for the account using the id
 					// and return the pointer to the account
 					return id;
@@ -225,7 +225,7 @@ int HashWaitlist::remove(Command* command)
 					}
 					// get the id from the command
 					int id = stoi(command->spaceParser(1).at(0));
-					
+
 					// delete the temp next data
 					delete temp->next->data;
 					// set temp next data to nullptr
@@ -234,7 +234,7 @@ int HashWaitlist::remove(Command* command)
 					temp->next = temp->next->next;
 					// set temps next to nullptr
 					temp->next = nullptr;
-					
+
 					// search for the account using the id
 					// and return the pointer to the account
 					return id;
@@ -245,7 +245,7 @@ int HashWaitlist::remove(Command* command)
 }
 
 // check if the passed in command is in the waitlist
-bool HashWaitlist::isInWaitlist(Command* command)
+bool HashWaitlist::isInWaitlist(Commands* command)
 {
 	// search the hash and return true if the command is found
 	if (search(command) != nullptr)
@@ -259,7 +259,7 @@ bool HashWaitlist::isInWaitlist(Command* command)
 }
 
 // is dependent on the genre type
-int HashWaitlist::hashFunction(Command* command)
+int HashWaitlist::hashFunction(Commands* command)
 {
 	// check if the command pointer is empty
 	if (command == nullptr)
